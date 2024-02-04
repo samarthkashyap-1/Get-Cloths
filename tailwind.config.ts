71056df77884b34b91516e625c3ac11d904d953b
samarth kashyap <samarthkashyap318@gmail.com>
2024-02-04 21:07:18 +0530

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      sec: "#333333",
+      tert: "#D3D3D3",
+      prim: "#9966CC",
+    },
   },
   plugins: [],
 };
